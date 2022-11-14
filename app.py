6e00f079b839cf91018fbee1d8709f44f6f5400b
jerome <jeromebrown97@gmail.com>
2022-11-13 20:43:20 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,77 @@
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie
+
+st.set_page_config(page_title="Jerome Brown", page_icon=":rocket:", layout="wide")
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None 
+    return r.json()
+
+
+def local_css(file_name):
+    with open(file_name) as f: 
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+        
+local_css("style/style.css")
+
+lottie_coding = load_lottieurl("https://assets10.lottiefiles.com/packages/lf20_vbhx85ve.json")
+
+
+with st.container():
+    st.subheader("Hi, I am Jerome :wave:")
+st.title("A innovator from the midwest :beers: ")
+st.write("I am passionate about using technology to solve complex problems")
+
+
+with st.container():
+        st.write("---")
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.header("What I do")
+            st.write("##")
+            st.write(
+    """
+    Build things
+    
+    
+    
+    
+    """
+   
+                
+                
+            )
+            
+            
+with right_column:
+    st_lottie(lottie_coding, height=300, key="coding")
+    
+    
+    
+with st.container():
+    st.write("---")
+    st.header("Get In Touch With Me!")
+    st.write("##")
+    
+    contact_form = """
+<form action="https://formsubmit.co/jbreview@outlook.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+    <input type="text" name="name" placeholder="Your name" required>
+    <input type="email" name="email" placeholder="Your email" required>
+    <textarea name="message" placeholder="Your message here" required></textarea> 
+    <button type="submit">Send</button>
+</form>
+"""
+
+    left_column, right_column = st.columns(2)
+    
+    with left_column:
+        
+        st.markdown(contact_form, unsafe_allow_html=True)
+        with right_column:
+            st.empty()
+            
+    
