dac6048b7077d697e7770ee857a2fc79957bd569
Jerome Brown <jeromebrown97@gmail.com>
2022-11-14 22:00:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,10 @@ with st.container():
             
 # with right_column:
 #     st_lottie(lottie_coding, height=300, key="coding")
-    
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
     
     
 with st.container():
