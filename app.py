cd8f945709741c0e48175146a82e0f498cc2c595
Jerome Brown <jeromebrown97@gmail.com>
2022-11-14 21:35:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ with st.container():
         st.write("---")
         left_column, right_column = st.columns(2)
         with left_column:
-            st.header("Classes")
+            st.header("Reading & Classes :book:")
             st.write("##")
             st.write(
     """
